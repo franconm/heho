f97bd55c36a54bcf32a7a5967da7fade683e37de
Mathieu Francon <franconmathieu1@gmail.com>
2023-06-06 00:07:50 +0200

    try 2

--- reset.php
+++ reset.php
@@ -1,10 +1,33 @@
 <?php
-// reset les variables de session
+// Finds all server sessions
+session_start();
+// Stores in Array
+$_SESSION = array();
+// Swipe via memory
+if (ini_get("session.use_cookies")) {
+    // Prepare and swipe cookies
+    $params = session_get_cookie_params();
+    // clear cookies and sessions
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+// Just in case.. swipe these values too
 ini_set('session.gc_max_lifetime', 0);
 ini_set('session.gc_probability', 1);
 ini_set('session.gc_divisor', 1);
+// Completely destroy our server sessions..
+session_destroy();
+?>
 
 
+<?php
 // reset files of IOT variables
 $data = 0;
 
@@ -13,5 +36,4 @@ file_put_contents($file1, $data);
 
 $file2 = 'model/sound_start.txt';
 file_put_contents($file2, $data);
-
 ?>
