4d16a68faf5f6c297f7c64ca1dabb81e3df865c8
Mathieu Francon <franconmathieu1@gmail.com>
2023-06-05 23:55:25 +0200

    add reset page

---
+++ reset.php
@@ -0,0 +1,30 @@
+<?php
+// démarre la session
+session_start();
+
+// supprime toutes les variables de session
+$_SESSION = array();
+
+// supprime le cookie de session
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// détruit la session
+session_destroy();
+
+
+// reset les fichiers de variables IOT
+$data = 0;
+
+$file1 = 'model/relique_finale.txt';
+file_put_contents($file1, $data);
+
+$file2 = 'model/sound_start.txt';
+file_put_contents($file2, $data);
+
+?>
