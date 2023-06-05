6ffe47081b241d5939d18a1a19e335f54c9ed516
Mathieu Francon <franconmathieu1@gmail.com>
2023-06-06 00:05:55 +0200

    try to remove sessions files

--- reset.php
+++ reset.php
@@ -1,24 +1,11 @@
 <?php
-// démarre la session
-// session_start();
-
-// supprime toutes les variables de session
-$_SESSION = array();
-
-// supprime le cookie de session
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-
-// détruit la session
-session_destroy();
+// reset les variables de session
+ini_set('session.gc_max_lifetime', 0);
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 1);
 
 
-// reset les fichiers de variables IOT
+// reset files of IOT variables
 $data = 0;
 
 $file1 = 'model/relique_finale.txt';
